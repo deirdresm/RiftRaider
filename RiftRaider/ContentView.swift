5efe1980f0b0950d6669f1b3b5f3cd74c8e0cffc
Deirdre Saoirse Moen <deirdre@deirdre.net>
2019-12-19 11:42:41 -0800

    Initial Commit

---
+++ RiftRaider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RiftRaider
+//
+//  Created by Deirdre Saoirse Moen on 12/19/19.
+//  Copyright © 2019 Deirdre Saoirse Moen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
